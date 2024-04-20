9df5f70c203f8d68dce8857966ce0d4f85251f17
reak97 <sebamontero15@gmail.com>
2024-04-20 17:12:55 -0300

    folder back and front, config express and react

---
+++ frontend/src/components/Home.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Home = () => {
+  return (
+    <div className="container mt-4">
+      <h1>PUTO EL QUE LEE XD</h1>
+      <p>¡Aquí puedes encontrar PORNO GAY increíble!</p>
+      <button className="btn btn-primary">Haz clic aquí</button>
+    </div>
+  );
+};
+
+export default Home;
