7ad7a46ac65094fa5b5712de2e6aa7225f32fc6f
reak97 <sebamontero15@gmail.com>
2024-05-09 21:52:33 -0300

    navbar

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -4,11 +4,14 @@ import "./Web.css";
 export default function Header() {
     return (
       <header> 
-        <Navbar/>
+        
         <figure>
+          
           <div className='logo'>
             <img src={logo} alt="Logo Recipe Book	" className='w-50'/>
           </div>
+
+          <Navbar/>
         </figure>
       </header>
     );
--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,5 +1,20 @@
-export default function Navbar(){
-    return(
-        <p>Hola</p>
-    )
+export default function Navbar() {
+    return (
+    <nav> 
+        <section className="nav-bar">
+            <ul className="ul">
+                <li>
+                    Recetas
+                </li>
+                <li>
+                    Contactos
+                </li>
+                <li>
+                    VideosXXX
+                </li>
+            </ul>
+           
+        </section>
+    </nav>
+        );
 }
